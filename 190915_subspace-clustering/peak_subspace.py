a490b1d241540b085df87869b4cb03db6bf8c97a
Jade <892545949@qq.com>
2019-10-01 23:49:26 +0800

    Update peak_subspace.py
    finish peak_subspace main program

--- 190915_subspace-clustering/peak_subspace.py
+++ 190915_subspace-clustering/peak_subspace.py
@@ -5,6 +5,7 @@ from distance_matrix import distance_matrix
 from density_peak import densityPeakRNN
 from cluster import series_to_centers
 from update_subspace import getHA
+from update_peak import update_peak
 
 # 1、get Robot Execution Failures lp1-5 data
 path = 'E:\\Jade\\time_series\\190808_MTS-clustering'
@@ -15,7 +16,7 @@ for file in data_name:
     f = h5py.File(filename,'r')
     X = f['train_x'][:]
     y = f['train_y'][:] # 1,2,3... np.array
-
+    
     # 获取正常（没有变量权值）的距离矩阵
     PATH = './data/' + file + '_distance.npy'
     if os.path.exists(PATH):
@@ -31,7 +32,7 @@ for file in data_name:
     R = X.shape[2]
 
     # 2.1 初始化峰值
-    center_label = densityPeakRNN(K,X,D)
+    center_label,density = densityPeakRNN(K,X,D)
     # 2.2 随机子空间
     np.random.seed(0)
     W = np.random.random((K,R))
@@ -39,18 +40,24 @@ for file in data_name:
     # 标准化，每一行相加等于1
     for i in range(K):
         W[i] = W[i] / s[i]
+    
+        
+    # 迭代循环
+    iter = 30
+    for i in range(iter):
+        # 3、样本分配到簇
+        # 该步骤要用到变量子空间
+        part = np.zeros(N)
+        in_cluster = [[] for i in range(K)]
+        for n in range(N):
+            ck = series_to_centers(X,n,center_label,W)
+            part[n] = ck
+            in_cluster[ck].append(n)
 
-    # 3、样本分配到簇
-    # 该步骤要用到变量子空间
-    part = np.zeros(N)
-    in_cluster = [[] for i in range(K)]
-    for n in range(N):
-        ck = series_to_centers(X,n,center_label,W)
-        part[n] = ck
-        in_cluster[ck].append(n)
-
-    # 4、更新子空间和峰值
-    # 4.1 更新子空间
-    W = getHA(part,in_cluster,N,K,R,center_label,X)
-    # 4.2 更新峰值
+        # 4、更新子空间和峰值
+        # 4.1 更新子空间
+        W = getHA(part,in_cluster,N,K,R,center_label,X)
+        # 4.2 更新峰值
+        center_label = update_peak(center_label,in_cluster,density,D)
+    return part
     
